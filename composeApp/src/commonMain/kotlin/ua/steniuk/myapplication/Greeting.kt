dbd783c945a190fa113abe471de6aaeff7f430d7
MarynaSteniuk <stenyukmarina1@gmail.com>
2026-03-11 18:49:35 +0200

    Видалено невикористаний код.

--- composeApp/src/commonMain/kotlin/ua/steniuk/myapplication/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package ua.steniuk.myapplication
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/ua/steniuk/myapplication/Platform.kt
+++
@@ -1,7 +0,0 @@
-package ua.steniuk.myapplication
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
